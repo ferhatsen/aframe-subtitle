fc65f5ae0a486ce09620c53f496424799cc44dd3
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2016-06-23 18:51:56 -0300

    maint: First attempt of integrating srt.
    * data.srt: New file.
    * index.html: New file.
    * index.js: New file.

---
+++ index.js
@@ -0,0 +1,58 @@
+AFRAME.registerComponent('subtitle', {
+  schema: {
+    bevelEnabled: { default: false },
+    bevelSize: { default: 8, min: 0 },
+    bevelThickness: { default: 12, min: 0 },
+    curveSegments: { default: 12, min: 0 },
+    font: { default: 'helvetiker' },
+    height: { default: 0.05, min: 0 },
+    size: { default: 0.5, min: 0 },
+    style: { default: 'normal', oneOf: [ 'normal', 'italics' ] },
+    text: { default: '' },
+    weight: { default: 'normal', oneOf: [ 'normal', 'bold' ] }
+  },
+
+  /**
+   * Called when component is attached and when component data changes.
+   * Generally modifies the entity based on the data.
+   */
+  update: function (oldData) {
+    if (oldData == null || this.data.src != oldData.src)
+    {
+      this.data.pop = Popcorn(this.data.timeline);
+      this.data.pop.parseSRT( this.data.srt, function(){});
+
+      var el = this.el;
+
+      this.data.pop.on("trackstart",
+                       function(e)
+                       {
+//                         el.setAttribute("text", e.text);
+                       });
+
+      this.data.pop.on("trackend",
+                       function(e)
+                       {
+//                        el.setAttribute("text", "");
+                       });
+
+       this.data.text = "u"
+
+    }
+
+    this.el.getOrCreateObject3D('mesh', THREE.Mesh).geometry = getTextGeometry(this.data);
+  },
+
+});
+
+function getTextGeometry (data) {
+  return new THREE.TextGeometry(data.text, data);
+}
+
+
+  /**
+   * We should write:
+   *     <a-entity timeline="#video" subtitle="text.srt"/>
+   * or
+   *     <a-subtitle src="text.srt" timeline="#video"/>
+   */
